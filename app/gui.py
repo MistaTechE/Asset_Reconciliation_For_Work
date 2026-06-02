2bf01170195f02c9fcafc1a39418107381a015d1
MistaTechE <74339419+MistaTechE@users.noreply.github.com>
2026-06-02 13:16:30 -1000

    Update gui.py
    making the gui and buttons work

--- app/gui.py
+++ app/gui.py
@@ -1,5 +1,5 @@
 #gui.py
-
+from tkinter import Tk, Button
 from app.csv_loader import load_checkouts, load_replacements, load_returns
 from app.reconciler import reconcile
 
@@ -12,3 +12,15 @@ def generate_report():
     result = reconcile(checkouts, replacements, returns)
 
     result.to_csv("data/output/students_to_call.csv", index=False)
+
+
+def run_app():
+    root = Tk()
+
+    Button(
+        root,
+        text="Generate Call List",
+        command=generate_report
+    ).pack()
+
+    root.mainloop()
