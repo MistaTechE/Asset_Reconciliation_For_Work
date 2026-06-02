bd7786a47dfaaa232001b2e18fb74a42d63e0e71
MistaTechE <74339419+MistaTechE@users.noreply.github.com>
2026-06-02 13:57:35 -1000

    Update csv_loader.py
    load the three csv's

--- app/csv_loader.py
+++ app/csv_loader.py
@@ -1,1 +1,11 @@
 #csv_loader.py
+import pandas as pd
+
+def load_checkouts():
+    return pd.read_csv("data/input/checkouts.csv", dtype=str)
+
+def load_replacements():
+    return pd.read_csv("data/input/replacements.csv", dtype=str)
+
+def load_returns():
+    return pd.read_csv("data/input/returns.csv", dtype=str)
