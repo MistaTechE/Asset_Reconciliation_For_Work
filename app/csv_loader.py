3a70fe902eb1772225c46333595cfb649e996544
MistaTechE <74339419+MistaTechE@users.noreply.github.com>
2026-06-12 13:47:04 -1000

    Update csv_loader.py

--- app/csv_loader.py
+++ app/csv_loader.py
@@ -1,12 +1,1 @@
-#csv_loader.py
-#no longer need this file
-#import pandas as pd
 
-#def load_checkouts():
-    #return pd.read_csv("data/input/checkouts.csv", dtype=str)
-
-#def load_replacements():
-    #return pd.read_csv("data/input/replacements.csv", dtype=str)
-
-#def load_returns():
-    #return pd.read_csv("data/input/returns.csv", dtype=str)
