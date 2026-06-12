3abe8ae3ef74a24de1fb3b4904cb532605da1165
MistaTechE <74339419+MistaTechE@users.noreply.github.com>
2026-06-12 12:45:03 -1000

    Update gui.py
    changed it so now files can be selected by the user

--- app/gui.py
+++ app/gui.py
@@ -1,13 +1,30 @@
 #gui.py
-from tkinter import Tk, Button, messagebox
+from tkinter import Tk, Button, messagebox, filedialog
 from app.csv_loader import load_checkouts, load_replacements, load_returns
 from app.reconciler import reconcile
+import pandas as pd
 
 
 def generate_report():
-    checkouts = load_checkouts()
-    replacements = load_replacements()
-    returns = load_returns()
+    checkouts_path = filedialog.askopenfilename(
+        title="Select Checkouts CSV"
+    )
+
+    replacements_path = filedialog.askopenfilename(
+        title="Select Replacements CSV"
+    )
+
+    returns_path = filedialog.askopenfilename(
+        title="Select Returns CSV"
+    )
+
+    if not checkouts_path or not replacements_path or not returns_path:
+        messagebox.showwarning("Cancelled", "File selection cancelled, must have three files.")
+        return
+
+    checkouts = pd.read_csv(checkouts_path, dtype=str)
+    replacements = pd.read_csv(replacements_path, dtype=str)
+    returns = pd.read_csv(returns_path, dtype=str)
 
     result = reconcile(checkouts, replacements, returns)
 
